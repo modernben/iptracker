1631357fad1f0073b42b631984f80cfb1dce5a1e
Ben <ben@modernmcguire.com>
2023-08-03 09:05:10 -0700

    ~ Clean up laravel app defaults

--- resources/views/livewire/poll.blade.php
+++
@@ -1,5 +0,0 @@
-<div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
-
-    <button type="button" wire:click="sendIt">Send It</button>
-</div>
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-use Native\Laravel\Facades\Shell;
-use Native\Laravel\Facades\System;
-use Native\Laravel\Facades\Window;
-use Native\Laravel\Facades\MenuBar;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,12 +10,3 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('openBrowser', function(Request $request){
-    Shell::openExternal($request->url);
-
-    dispatch(function(){
-        MenuBar::hide();
-    })->afterResponse();
-
-})->name('openBrowser');
